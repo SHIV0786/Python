bf5e5cf481b829b50c77dd97b2f2197fefad6ed4
Shivam Singh Rajawat <118413434+SHIV0786@users.noreply.github.com>
2022-12-03 01:25:35 +0530

    Update reaading_csv_data.py

--- reaading_csv_data.py
+++ reaading_csv_data.py
@@ -12,3 +12,28 @@ with open("weather_data.csv") as data_file:
             temperature.append(int(row[1]))
 
     print(temperature)
+
+    
+    
+    # read csv data using python pandas library
+import pandas as pd
+data = pd.read_csv("weather_data.csv")
+temp_list = data["temp"].to_list()
+print(data["temp"].max())
+# get data in columns
+print(data.condition)
+
+#get data in row
+print(data[data.day == "Monday"])
+
+print(data[data.temp == data["temp"].max()])
+
+
+print("==============================")
+# Create a dataframe from scratch
+data_dict = {
+    "students":["Any",'James',"Angela"],
+    "scores":[76,56,65]
+}
+data = pd.DataFrame(data_dict)
+data.to_csv("new_cs.csv")
