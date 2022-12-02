07d93649df4dff4a2de2ceb85e800fbfc6f04044
Shivam Singh Rajawat <118413434+SHIV0786@users.noreply.github.com>
2022-12-03 00:47:52 +0530

    Create reaading_csv_data.py

---
+++ reaading_csv_data.py
@@ -0,0 +1,14 @@
+# reading csv data in python
+#with open("weather_data.csv") as data_file:
+ #   data = data_file.readlines()
+#    print(data)
+import csv
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperature = []
+    for row in data:
+        #print(row) ---- to print data
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+
+    print(temperature)
